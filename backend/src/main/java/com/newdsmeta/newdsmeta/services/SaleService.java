b3db4212388353e4e3f2af2fd8cb387a85f4816b
Pedro Lauria <pedroclauria@gail.com>
2022-09-26 15:50:10 -0300

    LoginSystem

---
+++ backend/src/main/java/com/newdsmeta/newdsmeta/services/SaleService.java
@@ -0,0 +1,40 @@
+package com.newdsmeta.newdsmeta.services;
+
+import org.springframework.data.domain.Pageable;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+
+import com.newdsmeta.newdsmeta.entities.Sale;
+import com.newdsmeta.newdsmeta.repository.SaleRepository;
+
+@Service
+public class SaleService {
+	
+	
+	
+	@Autowired
+	private SaleRepository repository;
+	
+	
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+		
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		LocalDate min = minDate.equals("") ? today.minusDays(365): LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("") ? today: LocalDate.parse(maxDate);
+		
+		return repository.findSales	(min, max, pageable);
+	}
+	
+public Page<Sale> searchName(String seller_name, Pageable pageable) {
+
+		return repository.searchName(seller_name, pageable);
+	}
+	
+}
